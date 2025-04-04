7e65f9bded87ae8efd25138e213534da5794866f
kseungr11 <42666615+Skilila@users.noreply.github.com>
2025-04-04 21:42:36 +0900

    Title: 정수 명령 처리 5, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 250404/정수 명령 처리 5/process-numeric-commands-5.cpp
+++ 250404/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -4,42 +4,49 @@
 
 using namespace std;
 
-int N, num[10000];
-
-void command() {
-	
-	cin >> N;
-
-	int num, index;
-	string order;
-	vector<int>list;
-
-
-	for (int i = 0; i < N; i++) {
-		cin >> order;
-
-		if (order == "push_back") {
-			cin >> num;
-			list.push_back(num);
-		}
-		else if (order == "pop_back") {
-			if (!list.empty()) {
-				list.pop_back();
-			}
-		}
-		else if (order == "size") {
-			cout << list.size() << endl;
-		}
-		else if(order == "get") {
-			cin >> index;
-			if (index >= 1 && index <= list.size()) {
-				cout << list[index - 1] << endl;
-			}
-		}
-	}
-}
+int N;
+string command[10000];
+int num[10000];
 
 int main() {
-	command();
-	return 0;
+    //동적 배열 선언
+    vector<int>list;
+    cin >> N;
+
+    //명령어와 주어지는 정수를 구분하여 배열에 저장
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push_back" || command[i] == "get") {
+            cin >> num[i];
+        }
+    }
+
+    //탐색
+    for (int i = 0; i < N; i++) {
+
+        //push_back명령어 실행시 num[i]값을 list의 맨 뒤에 저장
+        if (command[i] == "push_back") {
+            list.push_back(num[i]);
+        }
+
+        //pop_back명령어 실행시 list가 비어있는지 확인 후 맨 뒤에있는 원소값 삭제
+        else if (command[i] == "pop_back") {
+            if (!list.empty()) {
+                list.pop_back();
+            }
+        }
+
+        //size명령어 실행시 list의 크기를 출력
+        else if (command[i] == "size") {
+            cout << list.size() << endl;
+        }
+
+        //get명령어 실행시 num[i]가 1보다크고 list의 크기보다 작으면 list의 원소값출력
+        else if (command[i] == "get") {
+            if (num[i] >= 1 && num[i] <= list.size()) {
+                cout << list[num[i] - 1] << endl;
+            }
+        }
+    }
+    return 0;
 }
